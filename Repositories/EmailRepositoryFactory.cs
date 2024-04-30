c8376db765dac7beace54e1628dd5cbb9297ae7f
chamara <chamara_sachintha@yahoo.com>
2024-04-30 18:46:14 +1000

    Added comments to the EmailRepositoryFactory

--- Repositories/EmailRepositoryFactory.cs
+++ Repositories/EmailRepositoryFactory.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReadingEmailsGraphAPI.Repositories
 {
     public class EmailRepositoryFactory : IEmailRepositoryFactory
     {
+
         public IEmailRepository CreateEmailRepository(IConfiguration config)
         {       
             return new EmailRepository(config);
